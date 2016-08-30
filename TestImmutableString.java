b9c90220daa99df05eaf2707e4e3e536a5a69a74
MaryAnnThomas <maryannthomas18@gmail.com>
2016-08-30 10:30:50 +0530

    Create TestImmutableString.java

---
+++ TestImmutableString.java
@@ -0,0 +1,22 @@
+package testimmutablestring;
+
+/**
+ *
+ * @author sony
+ */
+public class TestImmutableString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s = "Sachin";
+        s.concat("Tendulkar");
+        System.out.println(s);
+    }
+    
+}
+Output:
+run:
+Sachin
+BUILD SUCCESSFUL (total time: 1 second)
